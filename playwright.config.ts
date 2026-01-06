e1649714d50f299e9864fef38913bf67c9335c1f
rahulk152 <rahulkumaraiet@yahoo.in>
2026-01-06 16:33:11 +0530

    hooks, annotation, grouping, screenshot, parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  // grep:/@sanity/, //specify the tag by Rahul 
+  //grepInvert:/@regresion/,
 
   //To change timeout globally for all tests(default is 30000 ms ie 30 sec)
   //timeout:60000,
@@ -25,17 +27,22 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  //Retry locally by Rahul
+  //retries:3,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot:'only-on-failure', // capture screenshot by Rahul
+    video:'retain-on-failure', // Record video by rahul
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     testIdAttribute: 'data-pw' //configure data-testid
   },
 
